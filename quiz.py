30475ddad7ad7d73cace7bac85e5292ae1a68878
Ravisankar-46 <nvsravisankar@gmail.com>
2025-12-17 17:46:30 +0530

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,29 @@
+print("Welcome to my computer quiz!")
+x=input("Do you want to play? ")
+if x.lower()!="yes":
+   quit()
+
+print("First Question")
+y=input("Full Form of CPU: ")
+if(y.lower()=='centeral processing unit'):
+      print("Correct Answer")
+else:
+     print("Wrong Answer")
+
+y=input("Full Form of GPU: ")
+if(y.lower()=='graphical processing unit'):
+      print("Correct Answer")
+else:
+     print("Wrong Answer")
+
+y=input("Full Form of RAM: ")
+if(y.lower()=='random access memory'):
+      print("Correct Answer")
+else:
+     print("Wrong Answer")
+
+y=input("Full Form of USB: ")
+if(y.lower()=='universal serial bus'):
+      print("Correct Answer")
+else:
+     print("Wrong Answer")
